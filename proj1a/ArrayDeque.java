2f44e28617c9e3538e9dd43af9499c362346c078
Yuxuan Wang <yuxuan.www@163.com>
2020-05-23 21:56:02 +0800

    Project 1a finished

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,107 @@
+public class ArrayDeque<Item> {
+
+    private Item[] list;
+
+    private int size;
+
+
+    public ArrayDeque() {
+        list = (Item[]) new Object[100];
+        size = 0;
+    }
+
+
+    public ArrayDeque(ArrayDeque another) {
+        list = (Item[]) new Object[another.list.length];
+        System.arraycopy(another, 0, list, 0, another.list.length);
+        size = another.size;
+    }
+
+
+    private double calcRatio() {
+        double ratio = size/list.length;
+        return ratio;
+    }
+
+
+    private void upSize() {
+        Item[] temp = (Item[]) new Object[size*2];
+        System.arraycopy(list, 0, temp, 0, size);
+        list = temp;
+    }
+
+
+    private void downSize() {
+        Item[] temp = (Item[]) new Object[list.length/2];
+        System.arraycopy(list, 0, temp, 0, size);
+        list = temp;
+    }
+
+
+    public void addFirst(Item x) {
+        if(size == list.length) {
+            upSize();
+        }
+        Item[] temp = (Item[]) new Object[list.length];
+        System.arraycopy(list,0, temp, 1, size);
+        temp[0] = x;
+        list = temp;
+        size++;
+    }
+
+
+    public void addLast(Item x) {
+        if (size == list.length) {
+            upSize();
+        }
+        list[size] = x;
+        size++;
+    }
+
+
+    public Item get(int index) {
+        return list[index];
+    }
+
+
+    public Item removeFirst() {
+        Item[] tempList = (Item[]) new Object[list.length];
+        Item temp = list[0];
+        System.arraycopy(list,1, tempList, 0, size);
+        list = tempList;
+        size--;
+        if (calcRatio() < 0.25) {
+            downSize();
+        }
+        return temp;
+    }
+
+
+    public Item removeLast() {
+        Item temp = get(size - 1);
+        list[size - 1] = null;
+        size--;
+        if (calcRatio() < 0.25) {
+            downSize();
+        }
+        return temp;
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -36,13 +36,14 @@ public class LinkedListDequeTest {
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
 		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
-		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+		///*
+		//LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+		ArrayDeque<String> lld1 = new ArrayDeque<>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst("front");
-		
+
 		// The && operator is the same as "and" in Python.
 		// It's a binary operator that returns true if both arguments true, and false otherwise.
 		passed = checkSize(1, lld1.size()) && passed;
@@ -58,7 +59,7 @@ public class LinkedListDequeTest {
 		lld1.printDeque();
 
 		printTestStatus(passed);
-		*/
+		//*/
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
@@ -67,8 +68,9 @@ public class LinkedListDequeTest {
 		System.out.println("Running add/remove test.");
 
 		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
-		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
+		///*
+		//LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
+		ArrayDeque<Integer> lld1 = new ArrayDeque<>();
 		// should be empty 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
@@ -81,7 +83,7 @@ public class LinkedListDequeTest {
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
-		*/
+		//*/
 	}
 
 	public static void main(String[] args) {
