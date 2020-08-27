75659e0711b0c4824f773131feabc89e9722a432
Yuxuan Wang <yuxuan.www@163.com>
2020-08-27 18:36:09 +0800

    Finished in Aug 27th, 2020

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,102 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import org.junit.Test;
+import org.junit.Assert.*;
 
 public class Percolation {
+    /** N-by-N grid */
+    private int[][] grid;
+    private int N;
+    /** unionfind */
+    private WeightedQuickUnionUF uf;
 
+    /** create N-by-N grid, with all sites initially blocked */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new RuntimeException("The N you input is invalid.");
+        }
+        this.grid = new int[N][N];
+        this.N = N;
+        this.uf = new WeightedQuickUnionUF(N*N + 1);
+    }
+
+    /** open the site (row, col) if it is not open already */
+    public void open(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException("The index is out of range.");
+        }
+        if (grid[row][col] == 0) {
+            grid[row][col] = 1;
+            unionNeighbors(row, col);
+            update();
+        }
+    }
+
+    /** update the grid status */
+    private void update() {
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                if (isFull(row, col)) { grid[row][col] = 2; }
+            }
+        }
+    }
+
+    /** is the site open? */
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException("The index is out of range.");
+        }
+        return (grid[row][col] >= 1);
+    }
+
+    /** is the site full? */
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException("The index is out of range.");
+        }
+        if (isOpen(row, col)) {
+            for (int i = 0; i < N; i++) {
+                if (uf.connected(i, row*N + col)) { return true; }
+            }
+        }
+        return false;
+    }
+
+    /** number of open sites */
+    public int numberOfOpenSites() {
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(grid[i][j] >= 1) { count++; }
+            }
+        }
+        return count;
+    }
+
+    /** helper method: union neighbors */
+    private void unionNeighbors(int row, int col) {
+        int index = row * N + col;
+        // connect left item
+        if (col > 0 && grid[row][col - 1] != 0) { uf.union(index - 1, index);}
+        // connect right item
+        if (col < N - 1 && grid[row][col + 1] != 0) { uf.union(index, index + 1);}
+        // connect above item
+        if (row > 0 && grid[row - 1][col] != 0) { uf.union(index - N, index);}
+        // connect below item
+        if (row < N - 1 && grid[row + 1][col] != 0) { uf.union(index, index + N);}
+    }
+
+    /** does the system percolate? */
+    public boolean percolates() {
+        for (int i = 0; i < N; i++) {
+            if ( isFull(N - 1, i) ) { return true; }
+        }
+        return false;
+    }
+
+    /** unit test */
+    public static void main(String[] args) {
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,38 @@
 package hw2;
 
 public class PercolationStats {
+    private int montcarloNum;
+    private int gridN;
+
+    public PercolationStats(int montcarloNum, int gridN) {
+        this.montcarloNum = montcarloNum;
+        this.gridN = gridN;
+    }
+
+    public double getStats(){
+        double[] results = new double[montcarloNum];
+        double result = 0;
+        for (int cnt = 0; cnt < montcarloNum; cnt++) {
+            PercolationFactory perF = new PercolationFactory();
+            Percolation perc = perF.make(gridN);
+            while(!perc.percolates()) {
+                int targetRow = (int)(Math.random()*gridN);
+                int targetCol = (int)(Math.random()*gridN);
+                perc.open(targetRow, targetCol);
+            }
+            results[cnt] = (double)perc.numberOfOpenSites() / (double)(gridN*gridN);
+            System.out.println(results[cnt]);
+        }
+        for (double i:results) {
+            result += i;
+        }
+        return result/montcarloNum;
+    }
+
+    public static void main(String[] args) {
+        PercolationStats ps = new PercolationStats(100, 100);
+        double result = ps.getStats();
+        System.out.println("Final result: " + result);
+    }
 
 }
